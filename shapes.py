076e8e496fe76eaeedcbd5efd9b0235a91ede37b
David Ventura <david@davidv.dev>
2025-02-15 19:31:24 +0100

    impl rotation & flip_h

--- shapes.py
+++ shapes.py
@@ -3,22 +3,21 @@ import svg
 from drawio_types import Geometry, Direction
 
 
-def curly(g: Geometry, direction: Direction) -> svg.Path:
+def curly(g: Geometry, direction: Direction, rotation: int, flip_h: bool) -> svg.Path:
     # Generated poking around on
     # https://sean.brunnock.com/SVG/SVGPathGenerator/
 
-    # TODO: Direction flips width/height!
-
-    # FIXME: this 'tactical' 3 makes the tip
-    # less sharp
-    right = g.x + g.width - 3
+    # right is used for the 'point', so tweaking this
+    # will make it longer/shorter, though that messes
+    # with the bounding box
+    right = g.x + g.width
     bot = g.y + g.height
     centerY = g.y + g.height / 2
 
     # TODO: curlyLeft/curlyRight are _customizable_
     # meaning that while width is constant
     # the position of the curly is not
-    curlyWidth = 20
+    curlyWidth = 10
     curlyLeft = g.x + (g.width - curlyWidth) / 2
     curlyRight = curlyLeft + curlyWidth
     # The curly itself is 10px wide
@@ -39,13 +38,20 @@ def curly(g: Geometry, direction: Direction) -> svg.Path:
         svg.MoveTo(g.x, bot),
         svg.Z(),
     ]
+
     rotation_angle = direction.rotation_angle()
+    rotation_angle += rotation
+    transforms: list[svg.Transform] = [svg.Rotate(rotation_angle)]
+    # FIXME the above code is .. backwards? so this flips it back
+    if not flip_h:
+        transforms.append(svg.Scale(-1, 1))
+
     return svg.Path(
         d=points,
         fill="none",
         stroke="rgb(0, 0, 0)",
         # TODO: arg
-        transform=[svg.Rotate(rotation_angle)],
+        transform=transforms,
         # rotate around the center
         style="transform-box: fill-box; transform-origin: center;",
     )
